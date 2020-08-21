0199c44c7ac528c4eab3eaeda3159849731202d2
Mike Smith <gooseisdead@mac.com>
2020-08-21 20:08:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,10 +4,10 @@ second_number = 566
 sum = first_number + second_number
 puts "#{sum}"
 
-difference = first_number + second_number
+difference = first_number - second_number
 puts "#{difference}"
 
-product = first_number + second_number
+product = first_number * second_number
 puts "#{product}"
 
 quotient = first_number / second_number
